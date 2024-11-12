584b4c64620ae5a9e2bb76b7a016908434a4ae8e
Mohamed Sohail Rajab <82951435+mosorajab@users.noreply.github.com>
2024-11-12 19:27:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ def investment_calculator():
     st.header("🚀 Investment Calculator")
     # Input fields for investment parameters
     current_age = st.number_input("Current Age", min_value=0, value=24)
-    retirement_age = st.number_input("Retirement Age", min_value=current_age + 1, value=50)
-    initial_investment_amount = st.number_input("Initial Investment Amount", min_value=0.0, value=10000.0, step=1000.0, format="%.2f")
-    initial_monthly_investment = st.number_input("Initial Monthly Investment", min_value=0.0, value=8000, step=100.0, format="%.2f")
-    annual_increase_rate = st.number_input("Annual Increase Rate of Monthly Investment (%)", min_value=10.0, value=00.00, step=0.1, format="%.2f")
+    retirement_age = st.number_input("Retirement Age", min_value=current_age + 1, value=55)
+    initial_investment_amount = st.number_input("Initial Investment Amount", min_value=0.0, value=300000.0, step=1000.0, format="%.2f")
+    initial_monthly_investment = st.number_input("Initial Monthly Investment", min_value=0.0, value=8000.0, step=100.0, format="%.2f")
+    annual_increase_rate = st.number_input("Annual Increase Rate of Monthly Investment (%)", min_value=0.0, value=10.0, step=0.1, format="%.2f")
     annual_return_rate = st.number_input("Annual Return Rate (%)", min_value=0.0, value=9.0, step=0.1, format="%.2f")
     
     if st.button("Calculate Investment"):
@@ -88,7 +88,7 @@ if __name__ == "__main__":
         }
         </style>
         <div class="footer">
-            <p>built by msr</p>
+            <p>built by mohamed sohail rajab</p>
         </div>
         """,
         unsafe_allow_html=True
